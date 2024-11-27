f597e7066b7f33a28aa98254492c910f382961a8
Thaily <lyvinhthai321@gmail.com>
2024-11-27 17:37:15 +0700

    update

--- index.ts
+++ index.ts
@@ -1,13 +1,42 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import { connect } from "./config/database";
+import { resolvers } from "./resolcers"
+import { typeDefs } from "./typeDefs";
+import http from "http";
+import { ApolloServer } from "@apollo/server";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { expressMiddleware } from "@apollo/server/express4";
+import cors from "cors";
+import bodyParser from "body-parser";
+
 const app: Express = express();
 const port: number = 3000;
+app.use(cors());
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 
-app.get("/article", (req: Request, res: Response) => {
-  res.json({
-    articles: []
-  })
-})
 
-app.listen(port , () => {
-  console.log(`website đang chạy localhot: http://localhost:${port}`)
-})
+// Kết nối MongoDB
+connect(process.env.MONGO_URL!);
+
+// GraphQL API
+
+const httpServer = http.createServer(app);
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
+
+
+(async () => {
+  await server.start();
+  app.use('/graphql', expressMiddleware(server));
+
+  httpServer.listen(port, () => {
+    console.log(`🚀 Server ready at http://localhost:${port}`);
+  });
+})();
